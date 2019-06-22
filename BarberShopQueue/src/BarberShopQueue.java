ce383a344e57aaaa4ac614a597b68508282d1f4f
AJMFactsheets <AJMFactsheets@gmail.com>
2019-06-22 18:08:57 -0500

    Added command for barbers to dequeue customers and fixed a few bugs.

--- BarberShopQueue/src/BarberShopQueue.java
+++ BarberShopQueue/src/BarberShopQueue.java
@@ -23,7 +23,7 @@ public class BarberShopQueue {
 		// Main program loop to accept commands
 		while (isStoreOpen) {
 			System.out.println("Welcome to our Barber Shop!");
-			System.out.println("Type \"Customer checkin <Name> <Preferred Barber> \" to begin!");
+			System.out.println("Type \"Customer checkin <Name>\" to begin!");
 			
 			
 			input = commandInputReader.nextLine();
@@ -71,16 +71,10 @@ public class BarberShopQueue {
 							if (delimitedInput[2].equals("")) {
 								processError();
 							}
+							
 							// Try to find Barber in list, if no in list, log an error to console
 							else {
-								int foundPosition = -1;
-								
-								for (int i = 0; i < workingBarbers.size(); i++) {
-									if (workingBarbers.get(i).getName().equals(delimitedInput[2])) {
-										foundPosition = i;
-										break;
-									}
-								}
+								int foundPosition = getBarberPosition(workingBarbers, delimitedInput[2]);
 								
 								if (foundPosition >= 0) {
 									workingBarbers.get(foundPosition).barberCheckIn();
@@ -98,23 +92,61 @@ public class BarberShopQueue {
 							if (delimitedInput[2].equals("")) {
 								processError();
 							}
+							
 							// Try to find Barber in list, if no in list, log an error to console
-							// TODO duplicated code
 							else {
-								int foundPosition = -1;
-								
-								// Find barber specified in list of working barbers
-								for (int i = 0; i < workingBarbers.size(); i++) {
-									if (workingBarbers.get(i).getName().equals(delimitedInput[2])) {
-										foundPosition = i;
-										break;
-									}
-								}
+								int foundPosition = getBarberPosition(workingBarbers, delimitedInput[2]);
 								
 								// If the barber was found, toggle working status
 								if (foundPosition >= 0) {
 									workingBarbers.get(foundPosition).barberCheckOut();
 								}
+								else {
+									System.err.println("Could not find barber named " + delimitedInput[2]);
+								}
+							}
+							break;
+						}
+						
+						// Called by Barber to get his next customer to work on
+						case "dequeue": {
+							
+							// Process third word
+							if (delimitedInput[2].equals("")) {
+								processError();
+							}
+							
+							// Try to find Barber in list, if no in list, log an error to console
+							else {
+								int foundPosition = getBarberPosition(workingBarbers, delimitedInput[2]);
+								
+								// Try to get the next customer for that barber
+								if (foundPosition >= 0) {
+									Customer currentCustomer = workingBarbers.get(foundPosition).getNextCustomer();
+									
+									// If there is a next customer
+									if (currentCustomer != null) {
+										System.out.println("Your next customer's name is " + currentCustomer.getName());
+										System.out.println("Your next customer would like the " + currentCustomer.getHairstyle().toString() + " hairstyle");
+										System.out.println("Please remember to run \"customer checkout <Name> before getting your next customer\"\n");
+									}
+									
+									// If there is not a next customer this means one of two things:
+									else {
+										
+										// There are no customers waiting in line
+										if (workingBarbers.get(foundPosition).isWorking()) {
+											System.out.println("You have no customers waiting for you! Time for a break!\n");
+										}
+										
+										// The barber is not clocked in as working
+										else {
+											System.err.println("Barber " + delimitedInput[2] + " is not currently working");
+											System.err.println("Please run \"barber checkin <Name>\" to begin accepting customers\n");
+										}
+									}
+								}
+								
 								else {
 									System.err.println("Could not find barber named " + delimitedInput[2]);
 								}
@@ -133,18 +165,20 @@ public class BarberShopQueue {
 				case "customer": {
 					
 					// Process second word
-					switch(delimitedInput[1]) {
+					switch (delimitedInput[1]) {
 						case "": {
 							processError();
 							break;
 						}
 						
 						case "checkin": {
+							
 							// Process third word
 							if (delimitedInput[2].equals("")) {
 								processError();
 							}
 							else {
+								
 								// For the new Customer Object to later be enqueued
 								String customerName = delimitedInput[2];
 								Barber customerBarber = null;
@@ -169,14 +203,16 @@ public class BarberShopQueue {
 										while (askName) {
 											System.out.println("Enter barber name:");
 											
-											String inputName = commandInputReader.nextLine().toLowerCase();
+											String inputName = commandInputReader.nextLine();
+											// Makes error message actually reflect user input
+											String sanitizedInputName = inputName.toLowerCase();
 											
 											// Search through all barbers to find the name the customer requests
 											for (int i = 0; i < workingBarbers.size(); i++) {
 												
 												// This block runs if the barber is found
 												// Duplicates are not handled, so the first barber in the list is taken.
-												if (workingBarbers.get(i).getName().equals(inputName)) {
+												if (workingBarbers.get(i).getName().equals(sanitizedInputName)) {
 													askName = false;
 													customerBarber = workingBarbers.get(i);
 													barberListPosition = i;
@@ -191,6 +227,7 @@ public class BarberShopQueue {
 										}
 										
 									}
+									
 									// Put customer in shortest barber queue
 									else if (result.startsWith("n")) {
 										askCustomer = false;
@@ -229,35 +266,49 @@ public class BarberShopQueue {
 										System.out.println("3. " + EnumHairstyle.SUPREME.toString());
 										
 										// Had to resort to parseInt since nextInt caused the Scanner buffer to bug out.
-										int hairstyleChoice = Integer.parseInt(commandInputReader.nextLine());
+										// Further split the input to parse the first character due to user testing issues.
+										String inputHairstyle = commandInputReader.nextLine().toLowerCase();
+										char parsedNumber = inputHairstyle.charAt(0);
 										
-										// Invalid number, so ask again
-										if (hairstyleChoice < 1 || hairstyleChoice > 3) {
-											System.err.println("Please enter 1, 2, or 3.");
+										// If the value is not a digit, print an error and ask for correct input
+										if (!(Character.isDigit(parsedNumber))) {
+											System.err.println("Please enter either number 1, 2, or 3.");
 										}
 										
-										// Valid number, so stop asking and assign hairstyle to customer
+										// The value was a digit, so try to parse it.
 										else {
-											askCustomer = false;
+											// Java is annoying sometimes isn't it?
+											int hairstyleChoice = Integer.parseInt(Character.toString(parsedNumber));
 											
-											switch (hairstyleChoice) {
-												case 1: {
-													customerHairstyle = EnumHairstyle.STANDARD;
-													break;
-												}
-												case 2: {
-													customerHairstyle = EnumHairstyle.PREMIUM;
-													break;
-												}
-												case 3: {
-													customerHairstyle = EnumHairstyle.SUPREME;
-													break;
-												}
-												default: {
-													throw new IllegalStateException("Improper Hairstyle Enum");
+											// Invalid number, so ask again
+											if (hairstyleChoice < 1 || hairstyleChoice > 3) {
+												System.err.println("Please enter 1, 2, or 3.");
+											}
+											
+											// Valid number, so stop asking and assign hairstyle to customer
+											else {
+												askCustomer = false;
+												
+												switch (hairstyleChoice) {
+													case 1: {
+														customerHairstyle = EnumHairstyle.STANDARD;
+														break;
+													}
+													case 2: {
+														customerHairstyle = EnumHairstyle.PREMIUM;
+														break;
+													}
+													case 3: {
+														customerHairstyle = EnumHairstyle.SUPREME;
+														break;
+													}
+													default: {
+														throw new IllegalStateException("Improper Hairstyle Enum");
+													}
 												}
 											}
 										}
+
 									}
 									
 									// Create Customer Object and add to appropriate Barber Queue
@@ -266,7 +317,7 @@ public class BarberShopQueue {
 									// Hooray! Finally got the customer in the system! Pray for no NPEs!
 									workingBarbers.get(barberListPosition).addCustomer(customer);
 									
-									System.out.println("\nThank you! You will be called when your Barber is ready.\n");
+									System.out.println("\nThank you " + customerName + "! You will be called when your Barber is ready.\n");
 								}
 								break;
 							}
@@ -278,7 +329,7 @@ public class BarberShopQueue {
 							if (delimitedInput[2].equals("")) {
 								processError();
 							}
-							else {
+							else {				
 								System.out.println("Thank you for visiting our Barber " + delimitedInput[2] + "!");
 								System.out.println("Have a nice day!\n");
 							}
@@ -301,6 +352,7 @@ public class BarberShopQueue {
 		
 		System.out.println("Thanks for using BarberShopQueue by AJMFactsheets!");
 	}
+
 	
 	/**
 	 * Reads the IDs and names of a bunch of barbers from a file
@@ -342,6 +394,26 @@ public class BarberShopQueue {
 		return barberList;
 		
 	}
+	
+	/**
+	 * Takes a List of working barbers and finds the position of a barber by name: O(N) runtime
+	 * Created to reduce code duplication and increase abstraction
+	 * 
+	 * @param workingBarbers List to search through
+	 * @param name Barber's name to search for
+	 * @return position of barber in workingBarbers; -1 if not in list
+	 */
+	private static int getBarberPosition(CustomArrayList<Barber> workingBarbers, String name) {
+		int foundPosition = -1;
+		
+		for (int i = 0; i < workingBarbers.size(); i++) {
+			if (workingBarbers.get(i).getName().equals(name)) {
+				foundPosition = i;
+				break;
+			}
+		}
+		return foundPosition;
+	}
 
 	/**
 	 * Code block to call whenever there is an error: could be improved to be more specific
@@ -349,5 +421,5 @@ public class BarberShopQueue {
 	private static void processError() {
 		System.err.println("Incorrect command! Please try again!");
 	}
-
+	
 }
